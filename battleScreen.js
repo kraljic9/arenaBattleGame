ca5a536b1381c33eb0722b721b224839a9a4d963
Pavle Kraljic <kraljicpavle9@gmail.com>
2025-10-27 21:33:43 +0100

    Added play game functionality and computer actions

--- battleScreen.js
+++ battleScreen.js
@@ -15,6 +15,14 @@ let pcHealth = document.querySelector("#computer-health");
 let pcAttack = document.querySelector("#computer-attack");
 let pcAbility = document.querySelector("#computer-ability");
 
+let attackBtn = document.querySelector("#attack-btn");
+let defendBtn = document.querySelector("#defende-btn");
+let specialBtn = document.querySelector("#special-btn");
+
+let attackBtnClicked;
+let defendBtnClicked;
+let specialBtnClicked;
+
 let raw = sessionStorage.getItem("playersChoice");
 let player;
 
@@ -90,3 +98,74 @@ if (rawPc) {
 
 console.log("Player:", player);
 console.log("Computer:", computer);
+
+function playGame() {
+  if (player.health >= 100) {
+    player.health = 100;
+  } else if (computer.health >= 100) {
+    computer.health = 100;
+  } else if (player.health <= 0) {
+    console.log(`Computer is winner`);
+    return;
+  } else if (computer.health <= 0) {
+    console.log("Pc is winner");
+    return;
+  }
+
+  attackBtn.addEventListener("click", function () {
+    attackBtnClicked = true;
+    player.attackOpponent(computer);
+    computerSelect();
+    attackBtnClicked = false;
+    return computer.health;
+  });
+
+  defendBtn.addEventListener("click", function () {
+    attackBtnClicked = true;
+    player.defende(computer);
+    computerSelect();
+    attackBtnClicked = false;
+  });
+
+  specialBtn.addEventListener("click", function () {
+    attackBtnClicked = true;
+    player.specialAbility();
+    computerSelect();
+    attackBtnClicked = false;
+  });
+
+  playerHealth.innerHTML = `Health❤️: ${player.health}`;
+  playerAttack.innerHTML = `Attack⚔️: ${computer.attack};`;
+  pcHealth.innerHTML = `Health❤️: ${computer.health}`;
+  pcAttack.innerHTML = `Attack⚔️: ${computer.attack}`;
+}
+
+function computerSelect() {
+  let computerAction = Math.floor(Math.random() * 3) + 1;
+
+  if (computerAction === 1) {
+    computer.attackOpponent(player);
+    console.log(
+      `Computer has attacked player, player takes ${computer.attack} damage`
+    );
+    return player.health;
+  } else if (computerAction === 2) {
+    computer.defende(player);
+    console.log(`Computer has blocked players attack`);
+  } else if (computerAction === 3) {
+    computer.specialAbility();
+    console.log(`Computer has used his special ability`);
+  }
+
+  playerHealth.innerHTML = `Health❤️: ${player.health}`;
+  playerAttack.innerHTML = `Attack⚔️: ${computer.attack};`;
+  pcHealth.innerHTML = `Health❤️: ${computer.health}`;
+  pcAttack.innerHTML = `Attack⚔️: ${computer.attack}`;
+
+  console.log(computerAction);
+}
+
+playGame();
+/*
+When buttons are clicked do a action attack, defende, ability after also set button clicked value to true after action is finished trigger computer choice of action, set button clicked value to false,    
+*/
